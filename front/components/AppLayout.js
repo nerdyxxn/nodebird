8035350c3ea9c7dc6ec5c8fc4652127cb2e17fa3
윤성 <yunyul1209@gmail.com>
2023-10-13 23:46:06 +0900

    feat : Link와 eslint 추가

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ProTypes from 'prop-types';
+import Link from 'next/link';
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>공통메뉴</div>
+      <Link href="/" legacyBehavior>
+        <a>nodebird</a>
+      </Link>
+      <Link href="/profile" legacyBehavior>
+        <a>프로필</a>
+      </Link>
+      <Link href="/signup" legacyBehavior>
+        <a>회원가입</a>
+      </Link>
       {children}
     </div>
   );
