d83e56df56bdde8efa875dbcb2b5c10a629b44cd
윤성 <yunyul1209@gmail.com>
2023-10-14 19:47:54 +0900

    feat : antd 사용하여 반응형 그리드 적용하기
    - AntD Menu 'children' Warning 에러 해결

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c457828daedc6e4d"; }
+/******/ 		__webpack_require__.h = function() { return "1a9c957832b4ae54"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import ProTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Input, Menu, Row, Col } from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <Menu mode="horizontal">
-        <Menu.Item>
-          <Link href="/" legacyBehavior>
-            <a>nodebird</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link href="/profile" legacyBehavior>
-            <a>프로필</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link href="/signup" legacyBehavior>
-            <a>회원가입</a>
-          </Link>
-        </Menu.Item>
-      </Menu>
-      {children}
+      <Menu
+        mode="horizontal"
+        items={[
+          { label: <Link href="/">Nodebird</Link>, key: '/' },
+          { label: <Link href="/profile">프로필</Link>, key: '/profile' },
+          { label: <Input.Search enterButton />, key: '/search' },
+          {
+            label: <Link href="/signup">회원가입</Link>,
+            key: '/signup',
+          },
+        ]}
+      ></Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}></Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://github.com/nerdyxxn" target="_blank" rel="noreferrer noopener">
+            Made my Nerdyxxn
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
